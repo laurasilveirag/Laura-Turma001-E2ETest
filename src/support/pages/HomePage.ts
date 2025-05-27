abd0524c2cdd38bcee103e592f6b9745a10962d9
Laura Silveira Goncalves <laura.73821@alunosatc.edu.br>
2025-05-27 20:28:49 -0300

    #004

--- src/support/pages/HomePage.ts
+++ src/support/pages/HomePage.ts
@@ -1,29 +1,32 @@
-import { Page, Locator } from '@playwright/test'; 
+import { Page, Locator } from '@playwright/test';
 import { HomePageElements } from '../elements/HomePageElements';
 import { BasePage } from './BasePage';
 
 export class HomePage extends BasePage {
   private elements: HomePageElements;
-  private firstProductWrapperToHover: Locator; 
+  private firstProductWrapperToHover: Locator;
+  private fourthProductWrapperToHover: Locator;
 
   constructor(page: Page) {
     super(page);
     this.elements = new HomePageElements(page);
-
     this.firstProductWrapperToHover = page
       .locator('.features_items .product-image-wrapper')
+      .first();
+
+    this.fourthProductWrapperToHover = page
+      .locator('div:nth-child(4) > .product-image-wrapper')
       .first();
   }
 
   async addFirstProductToCart(): Promise<void> {
     await this.firstProductWrapperToHover.hover();
-
-    await this.page.waitForTimeout(500); 
-
+    await this.page.waitForTimeout(500);
     await this.elements.firstProductOverlayAddToCartButton.click();
   }
 
   async addFourthProductToCart(): Promise<void> {
+    await this.fourthProductWrapperToHover.hover();
 
     await this.elements.fourthProductImageWrapperAddToCartButton.click();
   }
